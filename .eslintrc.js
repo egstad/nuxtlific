db42f75d294fae005435ce9d88f78f0999596c5c
Jordan Egstad <jordan@egstad.com>
2019-09-12 17:59:45 -0700

    linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,41 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   extends: [
-    '@nuxtjs',
-    'prettier',
-    'prettier/vue',
-    'plugin:prettier/recommended',
-    'plugin:nuxt/recommended'
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    "prettier/vue",
+    "plugin:prettier/recommended"
   ],
-  plugins: [
-    'prettier'
-  ],
-  // add your custom rules here
   rules: {
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    // Ignore unused args but require usage of all others
+    'no-unused-vars': ['warn', { 'args': 'none' }],
+    // v-html vue component dangerously renders HTML
+    'vue/no-v-html': 'off',
+    // No single if in an "else" block
+    'no-lonely-if': 2,
+    // No async function without await
+    'require-await': 2,
+    // Force dot notation when possible
+    'dot-notation': 2,
+    'no-var': 2,
+    // Force object shorthand where possible
+    'object-shorthand': 2,
+    // No useless destructuring/importing/exporting renames
+    'no-useless-rename': 2,
+    'vue/no-parsing-error': [2, {
+      'x-invalid-end-tag': false
+    }]
+  },
+  globals: {
+    $nuxt: true
+  },
+  parserOptions: {
+    parser: "babel-eslint"
   }
-}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,24 +1,18 @@
 <template>
   <div class="container">
     <div>
-      <logo />
-      <h1 class="title">
-        nuxtlific
-      </h1>
-      <h2 class="subtitle">
-        Prismic + Netlify + Nuxt.js
-      </h2>
+      <Logo />
+      <h1 class="title">nuxtlific</h1>
+      <h2 class="subtitle">Prismic + Netlify + Nuxt.js</h2>
       <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
+        <a href="#" target="_blank" class="asshole">Hello</a>
+
         <a
           href="https://github.com/nuxt/nuxt.js"
           target="_blank"
           class="button--grey"
+          >GitHub</a
         >
-          GitHub
-        </a>
       </div>
     </div>
   </div>
@@ -29,8 +23,8 @@ import Logo from '~/components/Logo.vue'
 
 export default {
   components: {
-    Logo
-  }
+    Logo,
+  },
 }
 </script>
 
